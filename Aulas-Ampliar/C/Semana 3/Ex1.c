9255a8c0d77647cdc05882554cbe58fe48c02987
SonViktor <viktordias306@gmail.com>
2025-10-20 00:07:20 -0300

    4º commit, Aqui eu adicionei um exercício na semana 3 com foco em tabuada, e na semana 4 somente um arquivo base.

--- Aulas-Ampliar/C/Semana 3/Ex1.c
+++ Aulas-Ampliar/C/Semana 3/Ex1.c
@@ -1,8 +1,25 @@
+/*
+    Escreva um código que faça a tabuada de multiplicação desejada pelo usuario.
+*/
+
+
 #include <stdio.h>
 
 int main() 
 {
-    printf("Hello World");
+    int numero, limite;
+
+    printf("Digite o número da tabuada desejada: ");
+    scanf("%d", &numero);
+
+    printf("Digite até que número deseja multiplicar: ");
+    scanf("%d", &limite);
+
+     for (int i = 1; i <= limite; i++)
+     {
+        printf("%d x %d = %d\n", numero, i, numero * i);
+     } 
+     
 
     return 0;
 }
